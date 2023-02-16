6f0a481989dcb51a89da08ac32a6dde792266460
PrinzeJ <amoahjojo91@gmail.com>
2023-02-16 06:40:11 -0800

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints - "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - prints - and that piece of art is useful - Dora Korpar, 2015-10-19,
  * followed by a newline, to standard error.
- * Return; Always 1.
+ * Return: 1 (Success)
  */
 int main(void)
 {
