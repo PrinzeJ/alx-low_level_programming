010b4b1aa56cec3e28cbf881b057c299444c1e55
PrinzeJ <amoahjojo91@gmail.com>
2023-02-16 02:07:07 -0800

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * 
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
