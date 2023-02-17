4469ff7754f909f499a20313e45b20cfb850386b
PrinzeJ <amoahjojo91@gmail.com>
2023-02-17 10:48:57 -0800

    Invention

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - A program that prints combination of numbers
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 1; y < 10; y++)
+		{
+			if (x < y && x != y)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+				if (x + y != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
