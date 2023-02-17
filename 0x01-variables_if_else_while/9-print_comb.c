88f537b17f1e3a14fa0866a95587b20ceb9bca37
PrinzeJ <amoahjojo91@gmail.com>
2023-02-17 10:18:00 -0800

    Patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,11 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
+		putchar(i + '0');
 		if (i != 9)
 		{
 			putchar(',');
 			putchar(' ');
-		}
-		else
-		{
-		putchar(i + '0');
 		}
 		putchar('\n');
 	}
