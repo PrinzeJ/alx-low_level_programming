cb93a488ba4aa61b111a4291348a25468bda06ed
PrinzeJ <amoahjojo91@gmail.com>
2023-02-17 08:13:43 -0800

    Alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints letters
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
