6acf03414c537f165c7e79f366ff0b0403c302ff
PrinzeJ <amoahjojo91@gmail.com>
2023-03-02 07:47:53 -0800

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,7 +3,6 @@
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
- *
  * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
@@ -14,6 +13,7 @@ char *cap_string(char *str)
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
+
 		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
 		    str[index - 1] == '\n' ||
